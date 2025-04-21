f8f6ec7122eca856ff604e63a414bfdfbc41232e
Nasikhun Amin <nasikhunamin1011@gmail.com>
2025-04-22 00:39:20 +0700

    feat: setup server and implement book creation endpoint
    - Add server.js for initializing Hapi server
    - Add routes.js to register API routes
    - Add handlers.js with addBookHandler logic
    - Implement POST /books with validations

---
+++ src/handler.js
@@ -0,0 +1,60 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (request, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading
+  } = request.payload;
+
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = pageCount === readPage;
+  const newBook = {
+    name, year, author, summary, publisher, pageCount, readPage, reading, id, insertedAt, updatedAt, finished,
+  };
+
+  books.push(newBook);
+
+  const isSucces = books.filter((book) => book.id === id).length > 0;
+
+  if (isSucces) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+};
+module.exports = { addBookHandler };
