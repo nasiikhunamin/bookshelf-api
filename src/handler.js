4cc8c644d40faea6130465fb62450a61e9e70db7
Nasikhun Amin <nasikhunamin1011@gmail.com>
2025-04-22 18:31:15 +0700

    feat: implement GET all books and GET book detail endpoints

--- src/handler.js
+++ src/handler.js
@@ -57,4 +57,37 @@ const addBookHandler = (request, h) => {
   }
 
 };
-module.exports = { addBookHandler };
+
+const getAllBooksHandler = () => ({
+  status: 'success',
+  data: {
+    books: books.map((book) => ({
+      id: book.id,
+      name: book.name,
+      publisher: book.publisher,
+    })),
+  },
+});
+
+const getBookByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const book = books.find((n) => n.id === id);
+
+  if (book) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan'
+  });
+  response.code(404);
+  return response;
+};
+module.exports = { addBookHandler, getAllBooksHandler, getBookByIdHandler };
